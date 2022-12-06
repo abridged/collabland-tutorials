5d7e05b7745daba1fd747c00613bd40774bd1061
agnes512 <agnes@collab.land>
2022-12-06 11:43:51 -0500

    fix: remove stores

--- token-gating-website/README.md
+++ token-gating-website/README.md
@@ -4,6 +4,10 @@ This tutorial demonstrates how to build a token gating website with Collab.Land 
 
 ## Run the site locally
 
+You will need to register an API key to access this Collab.Land endpoint. Also noticed that this endpoint requires specific scope (`token-gating`).
+
+Include the API key in `.env` file then run:
+
 ```
 yarn install
 yarn run build
--- token-gating-website/src/index.js
+++ token-gating-website/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './stores/store';
 import App from './App';
 import './styles/index.css';
 import './styles/collabland.css';
@@ -10,11 +8,9 @@ import './styles/output.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
